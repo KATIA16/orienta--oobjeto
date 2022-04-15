eeaadf11faf6bfb9b2afc9776f9529bf031ab5e9
Kátia Almeida <katiaalmeidams@gmail.com>
2022-04-15 07:49:29 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+class ContaBancaria {
+    constructor(agencia, numero, tipo, saldo){
+        this.agencia = agencia;
+        this.numero = numero;
+        this.tipo = tipo;
+        this._saldo = saldo;
+    }
+
+    get saldo(){
+        return this._saldo;
+    }
+    set saldo(valor){
+        this._saldo = valor;
+    }
+
+    sacar(valor){
+        if(valor > this._saldo){
+            return "Operação nagada";
+        }
+        this._saldo = this._saldo - valor;
+
+        return this._saldo;
+    }
+
+    depositar(valor){
+        this._saldo = this._saldo + valor;
+
+        return this._saldo;
+    }
+}
+
+class ContaCorrente {
+    
+}
